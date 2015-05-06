54c3db79daa2675549b9e7db3ac139652675f3c4
uimac <tori31001@gmail.com>
2015-05-07 02:54:24 +0900

    update test script

--- Release/Win32/bone_test.py
+++ Release/Win32/bone_test.py
@@ -11,6 +11,7 @@ import sys
 
 def test():
 	for obj_index in range(get_object_size()):
+		messagebox(get_object_filename(obj_index).encode('sjis'))
 		bone_count = get_bone_size(obj_index)
 		messagebox("bone count:" + str(bone_count))
 		for bone_index in range(bone_count):
