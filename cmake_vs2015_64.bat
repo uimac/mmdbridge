b758b4cbac5e417263ff58d5b3a2ab67378a49ad
uimac <tori31001@gmail.com>
2017-08-06 15:58:33 +0900

    fix cmake version in cmake_vs2015_64.bat

--- cmake_vs2015_64.bat
+++ cmake_vs2015_64.bat
@@ -1,4 +1,8 @@
-set CMAKE="%VCPKG_DIR%\downloads\cmake-3.8.0-win32-x86\bin\cmake.exe"
+@echo off
+for /F "usebackq" %%i in (`forfiles /P "%VCPKG_DIR%\downloads" /m cmake* /c "cmd /c if @isdir==TRUE echo @path"`) do set CMAKE_DIR=%%i
+@echo on
+
+set CMAKE="%CMAKE_DIR%\bin\cmake.exe"
 set BUILD_DIR="build_vs2015_64"
 
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
